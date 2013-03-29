5f829883c4fc6039930634d9430b405d24eb6b73
Paul Covington <paul.covington@gmail.com>
2013-03-29 17:00:32 -0500

    Very Basic WPF UI

---
+++ TimeSummary.UI.WPF/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TimeEntry.Models;
+
+namespace TimeSummary.UI.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnParse_Click( object sender, RoutedEventArgs e )
+        {
+            // Testing Logging
+            //logger.Info( "Test" );
+
+            string total = string.Empty;
+            List<TimeLineItem> lineItems = new List<TimeLineItem>();
+
+            foreach ( string timeEntry in txtInput.Text.Split( '\n' ) )
+            {
+                try
+                {
+                    if ( timeEntry != string.Empty && timeEntry != "\r" ) lineItems.Add( TimeLineItem.Parse( timeEntry ) );
+                }
+                catch
+                {
+                    MessageBox.Show( string.Format( "Couldn't parse: {0}", timeEntry ) );
+                }
+
+            }
+
+            StringBuilder outputString = new StringBuilder();
+
+            var timeGroups = from li in lineItems
+                             group li by li.ProjectName.ToUpper() into g
+                             orderby g.Key
+                             select new { ProjectName = g.Key, TotalHours = g.Sum( li => li.TimeSpentInHours() ) };
+
+
+
+            foreach ( var li in timeGroups )
+            {
+                // output the total time spent for the day on a bucket
+                outputString.AppendLine( string.Format( "{0,-5}{1:0.00} Hours", li.ProjectName, li.TotalHours ) );
+
+                var comments = from cli in lineItems
+                               where cli.ProjectName == li.ProjectName && cli.Comment != string.Empty
+                               select cli.Comment;
+
+                // output the comments for each line item on a seperate line
+                foreach ( var comment in comments )
+                {
+                    outputString.AppendLine( string.Format( "          {0}", comment ) );
+                }
+            }
+
+            // Output a summary line
+            outputString.AppendLine( "----------------------------------------------------------" );
+            outputString.AppendLine( string.Format( "Total Time Worked: {0:0.00} Hours", timeGroups.Sum( x => x.TotalHours ) ) );
+
+
+            txtOutput.Text = outputString.ToString();
+        }
+    }
+}
