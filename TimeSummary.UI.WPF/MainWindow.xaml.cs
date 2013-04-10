272b3a1a92ff4c5dec5a9b5da19fa1971fa37039
TheSned <dsnedecor@gmail.com>
2013-04-10 12:55:55 -0500

    Prevent multiple Help windows from showing up.

--- TimeSummary.UI.WPF/MainWindow.xaml.cs
+++ TimeSummary.UI.WPF/MainWindow.xaml.cs
@@ -4,21 +4,35 @@ using TimeSummary.UI.WPF.ViewModels;
 
 namespace TimeSummary.UI.WPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : MetroWindow
-    {
-        public MainWindow()
-        {
-            this.DataContext = new TimeEntryViewModel();
-            InitializeComponent();
-        }
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : MetroWindow
+  {
+    private HelpWindow _helpWindow;
 
-        private void txtHelp_Click( object sender, RoutedEventArgs e )
-        {
-            var helpWindow = new HelpWindow();
-            helpWindow.Show();
-        }
+    public MainWindow()
+    {
+      this.DataContext = new TimeEntryViewModel();
+      InitializeComponent();
     }
+
+    private HelpWindow HelpWindow
+    {
+      get
+      {
+        if (_helpWindow == null)
+        {
+          _helpWindow = new HelpWindow();
+          _helpWindow.Closed += (sender, args) => _helpWindow = null;
+        }
+        return _helpWindow;
+      }
+    }
+
+    private void txtHelp_Click(object sender, RoutedEventArgs e)
+    {
+      this.HelpWindow.Show();
+    }
+  }
 }
